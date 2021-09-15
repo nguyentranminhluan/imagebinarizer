d07b5a6792026f3db322690dec4bcd99c8a38528
DoanHoangAnhNguyen <anhndh1997@gmail.com>
2021-09-15 11:10:12 +0200

    Small change in print help function

--- App/CommandLineParsing.cs
+++ App/CommandLineParsing.cs
@@ -99,12 +99,12 @@ namespace ImageBinarizerApp
             Console.WriteLine("\t- Red threshold: {\"-rt\", \"-red\"}");
             Console.WriteLine("\t- Green threshold: {\"-gt\", \"-green\"}");
             Console.WriteLine("\t- Blue threshold: {\"-bt\", \"-blue\"}");
-            Console.WriteLine("\nInput path and output path are required arguments, where as others can be set automaticaly if not specify");
-            Console.WriteLine("\nOthers values need to be larger than 0");
+            Console.WriteLine("\nInput path and output path are required arguments, where as others can be set automaticaly if not specified.");
+            Console.WriteLine("\nOthers values need to be larger than 0. If needed, use: \n\t-1 to assign threshold default value. \n\t0 to assign width and height default value.");
             Console.WriteLine("\n- Example:");
-            Console.WriteLine("\tWith automatic RGB: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -width 32 -height 32");
-            Console.WriteLine("\tOnly Height need to be specify: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -height 32");
-            Console.WriteLine("\tPassing all arguments: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -width 32 -height 32 \n\t\t-red 100 -green 100 -blue 100");
+            Console.WriteLine("\t+ With automatic RGB: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -width 32 -height 32");
+            Console.WriteLine("\n\t+ Only Height need to be specify: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -height 32");
+            Console.WriteLine("\n\t+ Passing all arguments: \n\t\tdotnet ImageBinarizerApp --input-image c:\\a.png --output-image d:\\out.txt -width 32 -height 32 \n\t\t-red 100 -green 100 -blue 100");
         }
     }
 
