32d9009eef6e34532846bb073a87e6a7bcc2f179
Damir Dobric <ddobric@daenet.com>
2021-09-30 16:06:06 +0200

    code review

--- source/Lib/BinarizerParams.cs
+++ source/Lib/BinarizerParams.cs
@@ -13,30 +13,37 @@ namespace ImageBinarizerLib
         /// image width
         /// </summary>
         public int ImageWidth { get; set; } = 0;
+
         /// <summary>
         /// image height
         /// </summary>
         public int ImageHeight { get; set; } = 0;
+
         /// <summary>
         /// Red threshold
         /// </summary>
         public int RedThreshold { get; set; } = -1;
+
         /// <summary>
         /// Green threshold
         /// </summary>
         public int GreenThreshold { get; set; } = -1;
+
         /// <summary>
         /// Blue threshold
         /// </summary>
         public int BlueThreshold { get; set; } = -1;
+
         /// <summary>
         /// Grey threshold
         /// </summary>
         public int GreyThreshold { get; set; } = -1;
+
         /// <summary>
         /// Inverse the contrast of the image
         /// </summary>
         public bool Inverse { get; set; } = false;
+
         /// <summary>
         /// Calculate base on grey scale threshold
         /// </summary>
