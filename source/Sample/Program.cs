1ef0b435cfa09772090a8e820155695645510ad9
callmewolf98 <68897507+callmewolf98@users.noreply.github.com>
2021-10-25 16:43:55 +0200

    Add Sample 14,15 and 16

--- source/Sample/Program.cs
+++ source/Sample/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Daenet.ImageBinarizerLib;
 using Daenet.ImageBinarizerLib.Entities;
 using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 
 namespace Sample
 {
@@ -40,6 +43,10 @@ namespace Sample
             //Sample10();
             //Sample11();
             //Sample12();
+            //Sample14();
+            //Sample15();
+            //Sample16();
+
         }
 
         /// <summary>
@@ -49,16 +56,17 @@ namespace Sample
         public static void Sample1()
         {
             Console.WriteLine("Create the image binary as a string that can be used in the application with preset arguments.");
-            var config = new BinarizerParams{
+            var config = new BinarizerParams
+            {
                 InputImagePath = "..\\..\\..\\..\\CommonFiles\\daenet.png",
                 ImageWidth = 100,
                 Inverse = true,
-                GetContour = true                
+                GetContour = true
             };
 
             var img = new ImageBinarizer(config);
             Console.WriteLine(img.GetStringBinariy());
-        
+
         }
 
         /// <summary>
@@ -184,7 +192,7 @@ namespace Sample
 
             var img = new ImageBinarizer(config);
             img.Run();
-        }        
+        }
 
         /// <summary>
         /// This sample demonstrates how to create the image binary of the logo with default width is 70 to fit to console window then save it as a C# code that can be used in your application.
@@ -257,6 +265,97 @@ namespace Sample
 
             var img = new ImageBinarizer(config);
             img.Run();
+        }
+
+
+        /// <summary>
+        /// Create image binary for 2 version of the same image black&amp;white and normal.
+        /// </summary>
+        public static void Sample14()
+        {
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\imgblack.jpg",
+                OutputImagePath = ".\\imgblack.txt",
+                ImageWidth = 1024
+            };
+
+            var img = new ImageBinarizer(config);
+            img.Run();
+            config.InputImagePath = "..\\..\\..\\..\\CommonFiles\\imgnormal.jpg";
+            config.OutputImagePath = ".\\imgnormal.txt";
+            img.Run();
+
+        }
+
+        /// <summary>
+        /// This sample scale down the picture if the width is larger than 1024 to fit in the limit character per line of notepad app on window
+        /// </summary>
+        public static void Sample15()
+        {
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\imgnormal.jpg",
+                OutputImagePath = ".\\imgnormal.txt",
+                ImageWidth = Bitmap.FromFile("..\\..\\..\\..\\CommonFiles\\imgnormal.jpg").Width > 1024 ?
+                                                        1024 : Bitmap.FromFile("..\\..\\..\\..\\CommonFiles\\imgnormal.jpg").Width
+            };
+
+            Console.WriteLine(Path.GetFullPath(config.OutputImagePath));
+            var img = new ImageBinarizer(config);
+            img.Run();
+        }
+
+
+
+        /// <summary>
+        ///The problem of txt file is that the space between each line can only be edit in the txt reader. By app default,
+        ///there will be gap between each line, which make the ratio of height and width seem different from the source img.
+        ///With html file, the gap between each line can be modify to make the preview ratio remain unchanged. This help the demonstration of the app
+        ///look better.
+        /// </summary>
+        public static void Sample16()
+        {
+            string inputPath = "..\\..\\..\\..\\CommonFiles\\B&W2.jpg";
+            Console.WriteLine("Create a image binary with a black and white image with custom width and height and default RGB thresholds then save it to the text file");
+            int ognWidth = Bitmap.FromFile(inputPath).Width > 512 ? 512 : Bitmap.FromFile(inputPath).Width;
+            Console.WriteLine(ognWidth);
+
+            var config = new BinarizerParams()
+            {
+                InputImagePath = inputPath,
+                OutputImagePath = ".\\imgnormal.html",
+                ImageWidth = ognWidth
+            };
+
+            Console.WriteLine(Path.GetFullPath(config.OutputImagePath));
+            var img = new ImageBinarizer(config);
+            var stringHtml = img.GetStringBinariy().ToString();
+            string quote = "\"";
+            string htmlWrite = $@"<!DOCTYPE html> 
+<html>
+<header>
+<title>ImgBiResult</title>
+</header>
+<body>
+<p style={quote}font-family: monospace; font-size: 28px; line-height:14px{quote}>
+{stringHtml}
+</p>
+</body>
+</html>
+";
+
+            using (StreamWriter writer = File.CreateText(config.OutputImagePath))
+            {
+                writer.Write(htmlWrite);
+            }
+            //
+            //Open the ouput html file on default web browse.
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = ".\\imgnormal.html",
+                UseShellExecute = true
+            });
         }
     }
 }
