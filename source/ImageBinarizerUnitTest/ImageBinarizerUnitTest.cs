400cca0c3e82f27a9cec75e92fc13d843b29dc0f
Luan Nguyen <luan.nguyentranminh@stud.fra-uas.de>
2021-11-05 03:51:30 +0100

    add unittest cases

--- source/ImageBinarizerUnitTest/ImageBinarizerUnitTest.cs
+++ source/ImageBinarizerUnitTest/ImageBinarizerUnitTest.cs
@@ -1,6 +1,9 @@
 using Daenet.ImageBinarizerLib;
 using Daenet.ImageBinarizerLib.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ImageBinarizerUnitTest
 {
@@ -8,10 +11,10 @@ namespace ImageBinarizerUnitTest
     public class ImageBinarizerUnitTest
     {
         /// <summary>
-        /// Check if the custom width and height and the output width is the same
+        /// Check if the custom width and height is the same as the output string width and height
         /// </summary>
         [TestMethod]
-        public void TestCase1()
+        public void OutputSizeMustBeTheSameAsCustomSize()
         {
 
             //
@@ -27,19 +30,25 @@ namespace ImageBinarizerUnitTest
 
             };
             var img = new ImageBinarizer(config);
-            var output = img.GetStringBinary();
+            var outputString = img.GetStringBinary();
+            var outputArray = img.GetArrayBinary();
 
             //
-            //Assert
-            Assert.AreEqual(wd, output.Split('\n')[0].Length - 1);
-            Assert.AreEqual(hg, output.Split('\n').Length - 1);
+            //Is output string size equal
+            Assert.AreEqual(wd, outputString.Split('\n')[0].Length - 1);
+            Assert.AreEqual(hg, outputString.Split('\n').Length - 1);
+
+            //
+            //Is output array size equal
+            Assert.AreEqual(wd, outputArray.GetLength(0));
+            Assert.AreEqual(hg, outputArray.GetLength(1));
         }
 
         ///<summary>
         ///Compare the width and height between input and output without setting up the width and height
         /// </summary>
         [TestMethod]
-        public void TestCase2()
+        public void OutputSizeMustBeTheSameAsInputImageSizeByDefault()
         {
             var inputWidth = System.Drawing.Image.FromFile("..\\..\\..\\..\\CommonFiles\\daenet.png").Width;
             var inputHeight = System.Drawing.Image.FromFile("..\\..\\..\\..\\CommonFiles\\daenet.png").Height;
@@ -55,12 +64,188 @@ namespace ImageBinarizerUnitTest
             Assert.AreEqual(inputWidth, output.Split('\n')[0].Length - 1);
         }
         /// <summary>
-        /// 
+        /// Test if red threshhold work correctly with custom threshold value 
         /// </summary>
         [TestMethod]
-        public void Testcase3()
+        public void RedThresholdTest()
+        {
+            //
+            //Create a bitmap data that every pixel got the same color(RGB) value.
+            Bitmap bmp = new Bitmap(320, 320);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+
+                    //redSample.
+                    bmp.SetPixel(x, y, Color.FromArgb(255, 200, 2, 2));
+
+                }
+            }
+            bmp.Save("..\\..\\..\\..\\CommonFiles\\redSample.png");
+
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\redSample.png",
+                OutputImagePath = ".\\redTest.txt",
+                RedThreshold = 199,
+                GreenThreshold = 1,
+                BlueThreshold = 1,
+            };
+
+            var img = new ImageBinarizer(config);
+            var output = img.GetStringBinary();
+
+            //As the red value is larger than threshold value every pixel should got value 1
+            Assert.IsFalse(output.Contains("0"));
+
+            config.RedThreshold = 201;
+            img = new ImageBinarizer(config);
+            output = img.GetStringBinary();
+
+            //As the red value is less than threshold value every pixel should got value 0
+            Assert.IsFalse(output.Contains("1"));
+        }
+
+        [TestMethod]
+        public void GreenThresholdTest()
+        {
+            //
+            //Create a bitmap data that every pixel got the same color(RGB) value.
+            Bitmap bmp = new Bitmap(320, 320);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+
+                    //greenSample.
+                    bmp.SetPixel(x, y, Color.FromArgb(255, 2, 200, 2));
+
+                }
+            }
+            bmp.Save("..\\..\\..\\..\\CommonFiles\\greenSample.png");
+
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\greenSample.png",
+                OutputImagePath = ".\\greenTest.txt",
+                RedThreshold = 1,
+                GreenThreshold = 199,
+                BlueThreshold = 1,
+            };
+
+            var img = new ImageBinarizer(config);
+            var output = img.GetStringBinary();
+
+            //As the green value is larger than threshold value every pixel should got value 1
+            Assert.IsFalse(output.Contains("0"));
+
+            config.GreenThreshold = 201;
+            img = new ImageBinarizer(config);
+            output = img.GetStringBinary();
+
+            //As the green value is less than threshold value every pixel should got value 0
+            Assert.IsFalse(output.Contains("1"));
+
+        }
+
+        [TestMethod]
+        public void BlueThresholdTest()
+        {
+            //
+            //Create a bitmap data that every pixel got the same color(RGB) value.
+            Bitmap bmp = new Bitmap(320, 320);
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+
+                    //blueSample.
+                    bmp.SetPixel(x, y, Color.FromArgb(255, 2, 2, 200));
+
+                }
+            }
+            bmp.Save("..\\..\\..\\..\\CommonFiles\\blueSample.png");
+
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\blueSample.png",
+                OutputImagePath = ".\\blueTest.txt",
+                RedThreshold = 1,
+                GreenThreshold = 1,
+                BlueThreshold = 199,
+            };
+
+            var img = new ImageBinarizer(config);
+            var output = img.GetStringBinary();
+
+            //As the blue value is larger than threshold value every pixel should got value 1
+            Assert.IsFalse(output.Contains("0"));
+
+            config.BlueThreshold = 201;
+            img = new ImageBinarizer(config);
+            output = img.GetStringBinary();
+
+            //As the blue value is less than threshold value every pixel should got value 0
+            Assert.IsFalse(output.Contains("1"));
+        }
+
+        [TestMethod]
+        public void GreyThresholdTest()
         {
 
         }
+
+        /// <summary>
+        /// Compare the output between parameter Inverse = true and Inverse = false 
+        /// two outputs.
+        /// </summary>
+        [TestMethod]
+        public void IsInverse()
+        {
+            var configInverse = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\daenet.png",
+                OutputImagePath = ".\\daenet.txt",
+                Inverse = true
+
+            };
+
+
+            var config = new BinarizerParams()
+            {
+                InputImagePath = "..\\..\\..\\..\\CommonFiles\\daenet.png",
+                OutputImagePath = ".\\daenetInverse.txt",
+                Inverse = false
+            };
+            var img = new ImageBinarizer(config);
+            var imgInverse = new ImageBinarizer(configInverse);
+
+
+            var outputArray = img.GetStringBinary();
+            var outputArrayInverse = imgInverse.GetStringBinary();
+
+
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                if (outputArray[i] == '1')
+                {
+                    Assert.AreEqual('0', outputArrayInverse[i]);
+                }
+                else if (outputArray[i] == '0')
+                {
+                    Assert.AreEqual('1', outputArrayInverse[i]);
+                }
+
+            }
+        }
+
+        [TestMethod]
+        public void IsCodeCreated()
+        {
+
+        }
+
+
     }
 }
